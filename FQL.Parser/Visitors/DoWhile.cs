c9fa609ef0e7f86b6f36b22fa20dbbe25c008112
Rob <rob.hill@chevinfleet.com>
2023-08-24 20:20:46 +0100

    Refactor DoWhileLoop to handle breaks in the middle of a loop
    The code changes in this commit refactor the DoWhileLoop class in order to handle breaks that occur in the middle of a loop. Previously, the code would only check for breaks at the end of each iteration. Now, we manually iterate across each statement in the loop's block and check for breaks after every statement. This ensures that any break statements within the loop are properly handled.
    
    These changes improve the functionality and reliability of handling breaks within do-while loops.

--- FQL.Parser/Visitors/DoWhile.cs
+++ FQL.Parser/Visitors/DoWhile.cs
@@ -4,10 +4,29 @@ public partial class FQLVisitor
 {
     public override object VisitDoWhileLoop(FQLParser.DoWhileLoopContext context)
     {
+        StateManager.loopBreakStack.Push(false);
+
+        // to handle breaks in the middle of a loop, we need to manually iterate across the while loops children
+
         do
         {
-            Visit(context.statements());
+            // Manually visit each statement in the loop's block
+            foreach (var stmt in context.statements().children)
+            {
+                Visit(stmt);
+
+                if (StateManager.loopBreakStack.Peek()) // Check for break after every statement
+                    break;
+            }
+            if (StateManager.loopBreakStack.Peek()) // Check for break after loop's statement block
+                break;
         } while ((bool)Visit(context.boolExpression()));
+
+        StateManager.loopBreakStack.Pop();
+        //do
+        //{
+        //    Visit(context.statements());
+        //} while ((bool)Visit(context.boolExpression()));
 
         return null;
     }
